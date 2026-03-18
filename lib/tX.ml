632196ec13ace7afcef3962de5fcf2d40ac633cd
Pierre Alain <pierre.alain@tuta.io>
2026-03-18 15:43:52 +0100

    add writer extra slots too, set gso_tcpv4 to true, now we need to correctly read them on the shared ring

--- lib/tX.ml
+++ lib/tX.ml
@@ -29,6 +29,13 @@ module Extra = struct
   let get_extra_gso_size c = Cstruct.LE.get_uint16 c 2
   let get_extra_gso_type c = Cstruct.get_uint8 c 4
   let get_extra_gso_pad c = Cstruct.get_uint8 c 5
+
+  let set_extra_type c typ = Cstruct.set_uint8 c 0 typ
+  let set_extra_flags c flags = Cstruct.set_uint8 c 1 flags
+  let set_extra_gso_size c size = Cstruct.LE.set_uint16 c 2 size
+  let set_extra_gso_type c typ = Cstruct.set_uint8 c 4 typ
+  let set_extra_gso_pad c pad = Cstruct.set_uint8 c 5 pad
+
   let sizeof_extra = 8
 
   let read slot =
@@ -46,6 +53,15 @@ module Extra = struct
       Ok { typ; flags; gso_size; gso_type; gso_pad }
     ) else (
       Ok { typ; flags; gso_size=0; gso_type=0; gso_pad=0 }
+    )
+
+  let write t slot =
+    set_extra_type slot t.typ;
+    set_extra_flags slot t.flags;
+    if t.typ = 2 then ( (* NETIF_EXTRA_TYPE_GSO *)
+      set_extra_gso_size slot t.gso_size;
+      set_extra_gso_type slot t.gso_type;
+      set_extra_gso_pad slot t.gso_pad
     )
 end
 
